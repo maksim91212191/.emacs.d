f37d004b7e53386853623217f6438f0cc84a5100
Maxim Esikov <maksim91212191@gmail.com>
2020-12-14 17:41:07 +0300

    All what I need basicly

---
+++ elpa/cmake-mode-20201210.1159/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20201210.1159" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "9109475bfb9371e74c6ddbf64a66054e5aaea795")
---
+++ elpa/git-command-20191028.333/git-command-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-command" "20191028.333" "A Git Command-Line interface" '((term-run "0.1.4") (with-editor "2.3.1")) :commit "a773d40da39dfb1c6ecf2b0758aa370ddea8f06d" :keywords '("utility" "git") :authors '(("10sr <8slashes+el [at] gmail [dot] com>")) :maintainer '("10sr <8slashes+el [at] gmail [dot] com>") :url "https://github.com/10sr/git-command-el")
