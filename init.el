408c3f5d9365fb3859dbdb2a1d05c697a08a4fe0
Maxim Esikov <maksim91212191@gmail.com>
2021-01-23 22:01:17 +0300

    v23.01.21

--- init.el
+++ init.el
@@ -76,6 +76,32 @@
     (interactive)
     (find-file open_project))
 
+;;-----------------Russian hotkeys off-------------
+(defun reverse-input-method (input-method)
+  "Build the reverse mapping of single letters from INPUT-METHOD."
+  (interactive
+   (list (read-input-method-name "Use input method (default current): ")))
+  (if (and input-method (symbolp input-method))
+      (setq input-method (symbol-name input-method)))
+  (let ((current current-input-method)
+        (modifiers '(nil (control) (meta) (control meta))))
+    (when input-method
+      (activate-input-method input-method))
+    (when (and current-input-method quail-keyboard-layout)
+      (dolist (map (cdr (quail-map)))
+        (let* ((to (car map))
+               (from (quail-get-translation
+                      (cadr map) (char-to-string to) 1)))
+          (when (and (characterp from) (characterp to))
+            (dolist (mod modifiers)
+              (define-key local-function-key-map
+                (vector (append mod (list from)))
+                (vector (append mod (list to)))))))))
+    (when input-method
+      (activate-input-method current))))
+
+(reverse-input-method 'russian-computer)
+
 ;;-----------------Company--------------------
 
 (use-package company
@@ -191,6 +217,7 @@
  '(fci-rule-character-color "#202020")
  '(fci-rule-color "#202020")
  '(fringe-mode 4 nil (fringe))
+ '(gdb-non-stop-setting nil)
  '(helm-ff-lynx-style-map t)
  '(main-line-color1 "#1E1E1E")
  '(main-line-color2 "#111111")
